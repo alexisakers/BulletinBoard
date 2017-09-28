12de360e1e3d4572d2aa67525026b1475ec1fdb5
Alexis Aubry <me@alexaubry.fr>
2017-09-28 20:40:51 +0200

    Configure example Xcode project

---
+++ Example/Sources/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Alexis AUBRY on 28/09/2017.
+//  Copyright © 2017 Alexis Aubry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
