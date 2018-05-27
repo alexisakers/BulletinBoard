f6e69d683243b974dfaec4743e2b4a0dac12e5ad
Alexis Aubry <me@alexaubry.fr>
2018-05-27 16:25:50 +0200

    Update copyright

--- Example/ObjC/AppDelegate.m
+++ Example/ObjC/AppDelegate.m
@@ -1,6 +1,6 @@
 /**
  *  BulletinBoard
- *  Copyright (c) 2017 Alexis Aubry. Licensed under the MIT license.
+ *  Copyright (c) 2017 - present Alexis Aubry. Licensed under the MIT license.
  */
 
 #import "AppDelegate.h"
