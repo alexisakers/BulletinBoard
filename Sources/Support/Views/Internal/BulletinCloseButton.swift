72210a0b457448738fe65bfe18aaa2f60fe84d5c
Alexis Aubry <alexisonline@fastmail.com>
2020-05-14 16:14:58 -0400

    Remove project resources

--- Sources/Support/Views/Internal/BulletinCloseButton.swift
+++ Sources/Support/Views/Internal/BulletinCloseButton.swift
@@ -37,18 +37,16 @@ class BulletinCloseButton: UIControl, HighlighterTarget {
         // Content
 
         isAccessibilityElement = true
-        accessibilityLabel = NSLocalizedString("Close", tableName: "Localizable",
-                                               bundle: Bundle(for: BulletinCloseButton.self), comment: "")
-
+        accessibilityLabel = Bundle.UIKitCore.localizedString(forKey: "Close", value: "Close", table: nil)
+    
         // Layout
-
         addSubview(backgroundContainer)
         addSubview(closeGlyph)
 
         backgroundContainer.layer.cornerRadius = 14
-
-        let glyph = UIImage(named: "CloseGlyph", in: Bundle(for: BulletinCloseButton.self), compatibleWith: nil)!
-        closeGlyph.image = glyph.withRenderingMode(.alwaysTemplate)
+        
+        closeGlyph.image = UIImage.closeButton.withRenderingMode(.alwaysTemplate)
+        closeGlyph.contentMode = .scaleAspectFit
         closeGlyph.clipsToBounds = true
 
         backgroundContainer.isUserInteractionEnabled = false
@@ -119,4 +117,46 @@ class BulletinCloseButton: UIControl, HighlighterTarget {
 
     }
 
+}
+
+extension Bundle {
+    fileprivate static var UIKitCore: Bundle {
+        return Bundle(identifier: "com.apple.UIKitCore")!
+    }
+}
+
+extension UIImage {
+    fileprivate static var closeButton: UIImage {
+        let shape = UIBezierPath()
+        shape.move(to: CGPoint(x: 0.93, y: 30.21))
+        shape.addCurve(to: CGPoint(x: 0.97, y: 35.02), controlPoint1: CGPoint(x: -0.28, y: 31.44), controlPoint2: CGPoint(x: -0.35, y: 33.72))
+        shape.addCurve(to: CGPoint(x: 5.78, y: 35.06), controlPoint1: CGPoint(x: 2.29, y: 36.34), controlPoint2: CGPoint(x: 4.55, y: 36.3))
+        shape.addLine(to: CGPoint(x: 18.01, y: 22.84))
+        shape.addLine(to: CGPoint(x: 30.21, y: 35.04))
+        shape.addCurve(to: CGPoint(x: 35, y: 34.99), controlPoint1: CGPoint(x: 31.49, y: 36.34), controlPoint2: CGPoint(x: 33.7, y: 36.32))
+        shape.addCurve(to: CGPoint(x: 35.05, y: 30.21), controlPoint1: CGPoint(x: 36.33, y: 33.69), controlPoint2: CGPoint(x: 36.33, y: 31.48))
+        shape.addLine(to: CGPoint(x: 22.84, y: 18.01))
+        shape.addLine(to: CGPoint(x: 35.05, y: 5.79))
+        shape.addCurve(to: CGPoint(x: 35, y: 1), controlPoint1: CGPoint(x: 36.33, y: 4.51), controlPoint2: CGPoint(x: 36.33, y: 2.3))
+        shape.addCurve(to: CGPoint(x: 30.21, y: 0.95), controlPoint1: CGPoint(x: 33.7, y: -0.32), controlPoint2: CGPoint(x: 31.49, y: -0.32))
+        shape.addLine(to: CGPoint(x: 18.01, y: 13.15))
+        shape.addLine(to: CGPoint(x: 5.78, y: 0.93))
+        shape.addCurve(to: CGPoint(x: 0.97, y: 0.98), controlPoint1: CGPoint(x: 4.55, y: -0.28), controlPoint2: CGPoint(x: 2.27, y: -0.35))
+        shape.addCurve(to: CGPoint(x: 0.93, y: 5.79), controlPoint1: CGPoint(x: -0.33, y: 2.3), controlPoint2: CGPoint(x: -0.28, y: 4.55))
+        shape.addLine(to: CGPoint(x: 13.15, y: 18.01))
+        shape.addLine(to: CGPoint(x: 0.93, y: 30.21))
+        shape.close()
+        
+        let size = CGSize(width: 36, height: 36)
+        UIGraphicsBeginImageContext(size)
+        
+        defer {
+            UIGraphicsEndImageContext()
+        }
+        
+        UIColor.black.setFill()
+        shape.fill()
+        
+        return UIGraphicsGetImageFromCurrentImageContext()!
+    }
 }
