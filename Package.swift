5e7a2b3616248afaba0da1333421c1bc7e1b7eb3
Olli Tapaninen <ollitapa@gmail.com>
2020-09-25 20:35:36 +0300

    SPM also to require iOS 11
    As outlined by version 5 manifest.

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "BLTNBoard",
-    platforms: [.iOS(.v9)],
+    platforms: [.iOS(.v11)],
     products: [
         .library(name: "BLTNBoard", targets: ["BLTNBoard"]),
     ],
